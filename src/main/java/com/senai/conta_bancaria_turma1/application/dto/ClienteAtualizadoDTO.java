ea5f8c709fa1847a35bedd1d58182e769dd2debb
Ruan Macionil <macionilruan@gmail.com>
2025-10-07 11:15:42 -0300

    implementando exepition

--- src/main/java/com/senai/conta_bancaria_turma1/application/dto/ClienteAtualizadoDTO.java
+++
@@ -1,8 +0,0 @@
-package com.senai.conta_bancaria_turma1.application.dto;
-
-public record ClienteAtualizadoDTO(
-        String nome,
-        String cpf
-) {
-
-}
--- src/main/java/com/senai/conta_bancaria_turma1/application/dto/TransferenciaDTO.java
+++
@@ -1,9 +0,0 @@
-package com.senai.conta_bancaria_turma1.application.dto;
-
-import java.math.BigDecimal;
-
-public record TransferenciaDTO (
-        String contaDestino,
-        BigDecimal valor
-){
-}
